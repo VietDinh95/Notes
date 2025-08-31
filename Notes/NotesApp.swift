326de9bb002d5d8f8f984aa2ed2bb2c63926758e
VietDH3.AVI <vdinh@digitalaviation.com>
2025-08-31 13:38:23 +0700

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by VietDH3.AVI on 31/8/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
